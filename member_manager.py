e2ed60f876cea950f8c90b4a488abf5c057ce54c
gyoh6629 <gyoh6629@gmail.com>
2026-05-15 16:54:29 +0900

    Add files via upload

--- member_manager.py
+++ member_manager.py
@@ -7,7 +7,7 @@ class Member:
         self.__phone = phone
         self.__add = add
     def __str__(self):
-        return f'회원번호\t아이디\t\t비밀번호\t이름\t\t전화번호\t주소\n{self.__num}\t\t{self.__id}\t\t{self.__pw}\t\t{self.__name}\t\t{self.__phone}\t\t{self.__add}'
+        return f'{self.__num}\t\t{self.__id}\t\t{self.__pw}\t\t{self.__name}\t\t{self.__phone}\t\t{self.__add}'
     
     def modify(self, num, id, pw, name, phone, add):
         if num != None:
@@ -52,6 +52,8 @@ class MemberService:
         for find_member in self.__member_list:
             if find_member.get_num() == num:
                 return find_member
+        else:
+            return False
     def modify_member(self, num, id, pw, name, phone, add):
         for find_member in self.__member_list:
             if find_member.get_num() == num:
@@ -61,6 +63,8 @@ class MemberService:
                 find_member.modify(num, id, None, None, None, None)
         return True
     def leave_member(self, num):
+        if not any(remain_member.get_num() == num for remain_member in self.__member_list):
+            return False
         self.__member_list = [remain_member for remain_member in self.__member_list if remain_member.get_num() != num]
         self.__basic_list = [remain_member for remain_member in self.__basic_list if remain_member.get_num() != num]
         return True
@@ -70,6 +74,13 @@ def select_menu():
     print('===============================================================================')
     menu = int(input('선택> '))
     return menu
+def input_member(question):
+    while True:
+        answer = input(question).strip()
+        if not answer:
+            print('잘못된 값입니다. 다시 입력해주세요.')
+        else:
+            return answer
 
 ms = MemberService()
 while True:
@@ -78,36 +89,47 @@ while True:
         print('============')
         print('회원가입')
         print('============')
-        num = input('회원번호 입력 : ')
-        id = input('아이디 입력 : ')
-        pw = input('비밀번호 입력 : ')
-        name = input('이름 입력 : ')
-        phone = input('전화번호 입력 : ')
-        add = input('주소 입력 : ')
+        num = input_member('회원번호 입력 : ')
+        id = input_member('아이디 입력 : ')
+        pw = input_member('비밀번호 입력 : ')
+        name = input_member('이름 입력 : ')
+        phone = input_member('전화번호 입력 : ')
+        add = input_member('주소 입력 : ')
         if ms.join_member(num, id, pw, name, phone, add):
             print('결과 : 회원가입이 완료되었습니다.')
     elif select == 2:
         print('============')
         print('회원목록')
         print('============')
-        for member_list in ms.list_member():
-            print(member_list)
+        member_list = ms.list_member()
+        if not member_list:
+            print('현재 회원이 없습니다.')
+        else:
+            print(f'회원번호\t아이디\t\t비밀번호\t이름\t\t전화번호\t주소')
+            for ml in member_list:
+                print("-------------------------------------------------------------------------------------")
+                print(ml)
+            
     elif select == 3:
         print('================')
         print('회원상세정보')
         print('================')
         num = input('회원번호 입력 : ')
-        print(ms.list_member_details(num))
+        details = ms.list_member_details(num)
+        if not details:
+            print('존재하지 않는 회원번호입니다.')
+        else:
+            print(details)
     elif select == 4:
         print('================')
         print('회원정보수정')
         print('================')
-        num = input('회원번호 입력 : ')
-        id = input('아이디 입력 : ')
-        pw = input('비밀번호 입력 : ')
-        name = input('이름 입력 : ')
-        phone = input('전화번호 입력 : ')
-        add = input('주소 입력 : ')
+        num = input_member('회원번호 입력 : ')
+        id = input_member('아이디 입력 : ')
+        pw = input_member('비밀번호 입력 : ')
+        name = input_member('이름 입력 : ')
+        phone = input_member('전화번호 입력 : ')
+        add = input_member('주소 입력 : ')
         if ms.modify_member(num, id, pw, name, phone, add):
             print('결과 : 수정이 완료되었습니다.')
     elif select == 5:
@@ -117,5 +139,9 @@ while True:
         num = input('회원번호 입력 : ')
         if ms.leave_member(num):
             print('결과 : 탈퇴가 완료되었습니다.')
+        else:
+            print('존재하지 않는 회원번호입니다.')
     elif select == 0:
         break
+    else:
+        print('메뉴에 있는 번호를 선택해주세요.')
